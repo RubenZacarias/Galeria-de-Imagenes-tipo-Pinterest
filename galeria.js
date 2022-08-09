b5ca5c179247167e74b951758995ca58471e2bbb
ruben zacarias <rubenzacarias2022@gmail.com>
2022-08-09 20:00:09 -0300

    Subiendo Proyecto de Galeria de Imagenes tipo Pinteres

---
+++ galeria.js
@@ -0,0 +1,22 @@
+ const fullimgbox = document.getElementById("full-img-box");
+ const fullimg = document.getElementById("fullimg");
+  
+ 
+
+
+function openfullimg(reference){
+    fullimgbox.style.display = "flex";
+    
+    
+   
+    
+   
+   
+}
+
+function closeimg(){
+    fullimgbox.style.display = "none";
+}
+
+
+
